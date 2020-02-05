7140f26fc674b1de876d90be71736e7de16e7377
jchen293 <jenny0418kobe@gmail.com>
2020-02-05 17:10:26 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header></header>
+
+      <h1> Hello </h1>
+    </div>
+  );
+}
+
+export default App;
