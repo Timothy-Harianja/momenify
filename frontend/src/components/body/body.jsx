6c78ce43e957576f525502fe2097d87c7e9204b8
Timothy-Harianja <timothy.harianja@gmail.com>
2020-02-26 20:06:48 -0500

    Added the post-item, posts-container components to display the posts on the main page

--- frontend/src/components/body/body.jsx
+++ frontend/src/components/body/body.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
+import PostsContainer from '../posts-container/posts-container';
+
+import './body.css'
+
 class Body extends Component {
   render() {
-    return <div>this is a body</div>;
+    return ( 
+      <div className='home-page'>
+        <div className='main-posts-container'>
+          <PostsContainer/>
+        </div>
+        <div className='side-posts-container'>
+          <PostsContainer/>
+        </div>
+      </div>
+    );
   }
 }
 
