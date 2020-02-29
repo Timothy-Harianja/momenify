54ef282dae80e65f3e518a0d04afbd76763cadc9
jchen293 <jenny0418kobe@gmail.com>
2020-02-29 00:02:03 -0500

    fixed

--- backend/server.js
+++ backend/server.js
@@ -39,15 +39,15 @@ app.use("/api", signupRote);
 app.use("/api", loginRoute);
 app.use("/api", confirmationRoute);
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+// Set static folder
+app.use(express.static("frontend/build"));
 
-  // index.html for all page routes
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// index.html for all page routes
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+// }
 // launch our backend into a port
 
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
