55c729c3750c0bde0641288024b10e7ffeff226c
jchen293 <jenny0418kobe@gmail.com>
2020-02-29 00:04:57 -0500

    fixed

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use("/api", confirmationRoute);
 
 // if (process.env.NODE_ENV === "production") {
 // Set static folder
-app.use(express.static("frontend/build"));
+app.use(express.static("./frontend/build"));
 
 // index.html for all page routes
 app.get("*", (req, res) => {
