b0b5793c9b93ffb7cd5378acfb166c3f8e186ce5
Timothy-Harianja <timothy.harianja@gmail.com>
2020-03-10 18:27:42 -0400

    submitting all the UI for sprint 1

--- frontend/src/components/post-item/post-item.jsx
+++ frontend/src/components/post-item/post-item.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import './post-item.css'
+import './post-item.css';
+import Button from 'react-bootstrap/Button';
 
-const PostItem = ({imageUrl}) => (
-     <img className='post-item' src={imageUrl} alt='post'/>
+const PostItem = ({imageUrl, profileUrl}) => (
+      <div className='post-item-container'>
+          <div className='post-item-header'>
+               <img className='post-item-profile' src={profileUrl}/>
+               <span className='post-item-name'>Timothy</span>
+          </div> 
+          <img className='post-item-main-image' src={imageUrl} alt='post'/>
+          <div className='post-item-footer'>
+               <Button className='post-item-like' variant="primary">Like</Button>
+               
+          </div>
+     </div> 
+     
 );
 
 export default PostItem;
