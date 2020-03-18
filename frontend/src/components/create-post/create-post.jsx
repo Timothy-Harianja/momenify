405273b8ca5e3693a8768a4baa70de56791f264c
Dan Dan Zhao <dandanzhao@Dans-MacBook-Pro.local>
2020-03-17 23:52:11 -0400

    add more borders into formsin UI

--- frontend/src/components/create-post/create-post.jsx
+++ frontend/src/components/create-post/create-post.jsx
@@ -4,7 +4,7 @@ import pic from "./imageicon.png";
 import hash from "./hashtag.jpeg";
 import posticon from "./posticon.png";
 import axios from "axios";
-import ImageUploader from "react-images-upload";
+//import ImageUploader from "react-images-upload";
 
 function makeTime() {
   return new Date().getTime();
