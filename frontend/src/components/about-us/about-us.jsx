f2f949179f9e0b47dee6906abba7490341e79c5b
Dan Dan Zhao <dandanzhao@Dans-MacBook-Pro.local>
2020-03-18 13:24:39 -0400

    add career page

--- frontend/src/components/about-us/about-us.jsx
+++ frontend/src/components/about-us/about-us.jsx
@@ -5,27 +5,47 @@ class AboutUs extends Component {
   render() {
     return (
       <div className="about-us">
+        <br></br>
         <h1 id="header"> About Us</h1>
         <br></br>
+        <h2 id="h5">
+          • Momenify is a platform that allow you to connect with other people.
+        </h2>
         <body id="body">
-          Momenify is a platform that allow you to connect with other people.
+          Unlike most social media platform, where you have to register an
+          account and add people to see their posts. At this platform, you can
+          see everyone's moment around the world without creating an account!
+          You can also make new friends from our platform.
+        </body>{" "}
+        <h2 id="h5">• How to use?</h2>
+        <body id="body">
+          You can post three moments per day as anonymous without login or
+          signup. Once you register an account or login, you can do more thing,
+          such as like and comment someone's post, post unlimited moments per
+          day, and message someone!
         </body>
-        <h2 id="h5">Express yourself</h2>
+        <h2 id="h5">• Express yourself</h2>
         <body id="body">
-          Momenify is your friend circle. Post text messages, images, videos,
-          audios, anything that you want to post.
+          Momenify is your friend circle. Be creative, post moments that can
+          attract people's attention. Or you can just post something that is on
+          your mind!
         </body>
-        <h2 id="h5">Be yourself</h2>
+        <h2 id="h5">• Be yourself</h2>
         <body id="body">
-          Look at the posts that you are interested in. Post the comments about
-          these posts. Make a post about yourself and your life.
+          Look at the moments that you are interested in. Make comments about
+          these moments. Make a moment about yourself and your life.
         </body>
-        <h2 id="h5">Connect with your people</h2>
+        <h2 id="h5">• Connect with your people</h2>
         <body id="body">
-          Join in the friend circle around the world. See something that
-          interested you? Make a post about it and say your optional. You can
-          follow the people that you are interested in, comment about their
-          posts and even message with them.
+          If you are interested in someone's moments, why don't you message them
+          directly and make some new friends?
+        </body>
+        <br></br>
+        <h2 id="h5">• Developers</h2>
+        <body id="body">
+          Backend Developer: Junjie Chen, Xingyu Liu
+          <br></br>
+          Frontend Developer: Dandan Zhao, Timothy Harianja
         </body>
       </div>
     );
