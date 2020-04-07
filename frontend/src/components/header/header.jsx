860fed9decb0f08d827fbf64700307727f3d3170
jchen293 <jenny0418kobe@gmail.com>
2020-04-07 00:01:04 -0400

    changed icons

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -41,19 +41,19 @@ const Styles = styled.div`
 class Header extends Component {
   state = {
     userId: null,
-    postExpire: null
+    postExpire: null,
   };
   componentDidMount() {
-    axios.get("/api/loginRoute/session").then(res => {
+    axios.get("/api/loginRoute/session").then((res) => {
       this.setState({
         userId: res.data.userId,
-        postExpire: res.data.postInterval
+        postExpire: res.data.postInterval,
       });
     });
   }
 
   logout = () => {
-    axios.post("/api/loginRoute/logout").then(res => {});
+    axios.post("/api/loginRoute/logout").then((res) => {});
   };
   render() {
     return (
@@ -69,14 +69,69 @@ class Header extends Component {
               {this.state.userId != undefined ? (
                 <Nav>
                   <Nav.Item>
-                    <Nav.Link href="/message">Message</Nav.Link>
+                    <Nav.Link href="/message">
+                      <svg
+                        class="bi bi-chat-dots"
+                        width="2em"
+                        height="2em"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M2.678 11.894a1 1 0 01.287.801 10.97 10.97 0 01-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 01.71-.074A8.06 8.06 0 008 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 01-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 00.244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 01-2.347-.306c-.52.263-1.639.742-3.468 1.105z"
+                          clip-rule="evenodd"
+                        />
+                        <path d="M5 8a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0zm4 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                      </svg>
+                    </Nav.Link>
                   </Nav.Item>
                   <Nav.Item>
-                    <Nav.Link href="/accountpage">My Account</Nav.Link>
+                    <Nav.Link href="/accountpage">
+                      <svg
+                        class="bi bi-people-circle"
+                        width="2em"
+                        height="2em"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 008 15a6.987 6.987 0 005.468-2.63z" />
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 9a3 3 0 100-6 3 3 0 000 6z"
+                          clip-rule="evenodd"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 1a7 7 0 100 14A7 7 0 008 1zM0 8a8 8 0 1116 0A8 8 0 010 8z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </Nav.Link>
                   </Nav.Item>
                   <Nav.Item>
                     <Nav.Link href="/" onClick={() => this.logout()}>
-                      Logout
+                      <svg
+                        class="bi bi-power"
+                        width="2em"
+                        height="2em"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M5.578 4.437a5 5 0 104.922.044l.5-.866a6 6 0 11-5.908-.053l.486.875z"
+                          clip-rule="evenodd"
+                        />
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.5 8V1h1v7h-1z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
                     </Nav.Link>
                   </Nav.Item>
                 </Nav>
