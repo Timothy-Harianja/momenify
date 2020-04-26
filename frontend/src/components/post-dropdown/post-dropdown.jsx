b2b4a0cc2ad39ce4e10be4bc238d7a2f76af4a82
jchen293 <jenny0418kobe@gmail.com>
2020-04-26 14:59:05 -0400

    bug fixed on iOS video playback

--- frontend/src/components/post-dropdown/post-dropdown.jsx
+++ frontend/src/components/post-dropdown/post-dropdown.jsx
@@ -27,7 +27,7 @@ const PostDropdown = () => {
       </Dropdown.Toggle>
 
       <Dropdown.Menu className="dropdown-menu">
-        <Dropdown.Item href="#/action-1">
+        <Dropdown.Item>
           <svg
             class="bi bi-person-plus"
             width="1em"
@@ -49,7 +49,7 @@ const PostDropdown = () => {
           </svg>
           <span>Follow</span>
         </Dropdown.Item>
-        <Dropdown.Item href="#/action-2">
+        <Dropdown.Item>
           <svg
             class="bi bi-chat"
             width="1em"
@@ -66,7 +66,7 @@ const PostDropdown = () => {
           </svg>
           <span>Message</span>
         </Dropdown.Item>
-        <Dropdown.Item href="#/action-3">
+        <Dropdown.Item>
           <svg
             class="bi bi-x-circle"
             width="1em"
@@ -92,6 +92,20 @@ const PostDropdown = () => {
             />
           </svg>
           <span>Report</span>
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <svg class="svg-icon" viewBox="0 0 20 20">
+            <path
+              fill="none"
+              d="M7.083,8.25H5.917v7h1.167V8.25z M18.75,3h-5.834V1.25c0-0.323-0.262-0.583-0.582-0.583H7.667
+								c-0.322,0-0.583,0.261-0.583,0.583V3H1.25C0.928,3,0.667,3.261,0.667,3.583c0,0.323,0.261,0.583,0.583,0.583h1.167v14
+								c0,0.644,0.522,1.166,1.167,1.166h12.833c0.645,0,1.168-0.522,1.168-1.166v-14h1.166c0.322,0,0.584-0.261,0.584-0.583
+								C19.334,3.261,19.072,3,18.75,3z M8.25,1.833h3.5V3h-3.5V1.833z M16.416,17.584c0,0.322-0.262,0.583-0.582,0.583H4.167
+								c-0.322,0-0.583-0.261-0.583-0.583V4.167h12.833V17.584z M14.084,8.25h-1.168v7h1.168V8.25z M10.583,7.083H9.417v8.167h1.167V7.083
+								z"
+            ></path>
+          </svg>
+          <span>Delete</span>
         </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
