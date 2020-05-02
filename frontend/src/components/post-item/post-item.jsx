1fb5fd56b199c79151148a6f09a999dd4a9eb4a6
unknown <724718356@qq.com>
2020-05-02 14:48:14 -0400

    readmore and hide post text

--- frontend/src/components/post-item/post-item.jsx
+++ frontend/src/components/post-item/post-item.jsx
@@ -11,9 +11,12 @@ class PostItem extends React.Component {
       commentInputBox: false,
       commentNumber: 3,
       commentText: null,
-      postid: this.props.postid,
-      position: this.props.position,
-      userid: this.props.id,
+      // showingPost: "",
+      // postText1: "",
+      // postText2: "",
+      readMore: "Read all",
+      splitPosition: 300,
+      restText: false,
     };
   }
 
@@ -88,13 +91,57 @@ class PostItem extends React.Component {
     }
     return "unkown";
   };
+  formatPostText = (text) => {
+    var rowLen = text.replace(/ /g, "\u00a0").split("\n").length;
 
-  // changeFollowStatus = (newStatus) => {
-  //   this.setState({ followStatus: newStatus });
-  //   console.log("changed follow status");
-  // };
+    return text
+      .replace(/ /g, "\u00a0")
+      .split("\n")
+      .map((message, i) => {
+        if (rowLen === i + 1) {
+          //the last one: no <br/>
+          return <span>{message}</span>;
+        }
+
+        return (
+          <span>
+            {message} <br />
+          </span>
+        );
+      });
+  };
+
+  longPostText = () => {
+    var edgeLength = 300;
+    var text = this.props.text;
+    var textLength = text.length + (text.match(/\n/g) || []).length * 80; //text length plus number of "\n"*80
+    return textLength > edgeLength;
+  };
+
+  PostText = () => {
+    return this.longPostText() ? (
+      <div>
+        {this.formatPostText(
+          this.props.text.slice(0, this.props.splitPosition)
+        )}
+        {this.props.boolHide
+          ? null
+          : this.formatPostText(
+              this.props.text.slice(this.props.splitPosition)
+            )}
+        <div
+          className="show-more-footer-comment"
+          onClick={() => this.props.changeBoolReadAll(this.props.position)}
+        >
+          {this.props.boolHide ? <span> Read all</span> : <span> Hide </span>}
+        </div>
+      </div>
+    ) : (
+      <div> {this.formatPostText(this.props.text)}</div>
+    );
+  };
+
   render() {
-    //console.log("filename: ", this.props.file);
     return (
       <div className="post-item-container">
         <div className="post-item-header">
@@ -129,7 +176,7 @@ class PostItem extends React.Component {
           )}
           <span className="post-item-header-dropdown">
             <PostDropdown
-              userid={this.state.userid}
+              // userid={this.props.id}
               // changeFollowStatus={(e) => this.changeFollowStatus(e)}
               followStatus={this.props.followStatus}
               updateFollow={(e) => this.props.updateFollow(e)}
@@ -140,15 +187,7 @@ class PostItem extends React.Component {
           <span className="post-item-header-date">{this.props.postDate}</span>
         </div>
         <div className="post-item-description">
-          {this.props.text
-            .replace(/ /g, "\u00a0")
-            .split("\n")
-            .map((message) => {
-              if (message.length == 0) {
-                return <div> &nbsp;</div>;
-              }
-              return <div>{message}</div>;
-            })}
+          <this.PostText></this.PostText>
         </div>
 
         {this.props.file != null ? (
@@ -222,8 +261,8 @@ class PostItem extends React.Component {
                     commentNumber: this.state.commentNumber + 1,
                   });
                   this.props.giveComment({
-                    postid: this.state.postid,
-                    position: this.state.position,
+                    postid: this.props.postid,
+                    position: this.props.position,
                     postComment: this.state.commentText,
                   });
                   this.state.commentText = null;
