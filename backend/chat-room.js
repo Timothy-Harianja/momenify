cfa0f24fc1cf43e8079e1eb0d5f084b2d96dafb2
jchen293 <jenny0418kobe@gmail.com>
2020-05-09 22:43:25 -0400

    revert

--- backend/chat-room.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const ChatRoom = new Schema({
-  users: [],
-  messageList: [],
-});
-
-module.exports = mongoose.model("ChatRoom", ChatRoomSchema);
--- frontend/src/components/MessageList/index.js
+++ frontend/src/components/MessageList/index.js
@@ -4,17 +4,14 @@ import Message from "../Message";
 import moment from "moment";
 
 import "./MessageList.css";
-import io from "socket.io-client";
+
 
 const MY_USER_ID = "apple";
 
-let socket;
-
 export default function MessageList(props) {
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:3000/message";
+
   useEffect(() => {
-    socket = io();
     getMessages();
   }, []);
 
@@ -25,71 +22,71 @@ export default function MessageList(props) {
         author: "apple",
         message:
           "Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 2,
         author: "orange",
         message:
           "It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 3,
         author: "orange",
         message:
           "Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 4,
         author: "apple",
         message:
           "It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 5,
         author: "apple",
         message:
           "Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 6,
         author: "apple",
         message:
           "It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 7,
         author: "orange",
         message:
           "Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 8,
         author: "orange",
         message:
           "It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 9,
         author: "apple",
         message:
           "Hello world! This is a long message that will hopefully get wrapped by our message bubble component! We will see how well it works.",
-        timestamp: new Date().getTime(),
+        timestamp: new Date().getTime()
       },
       {
         id: 10,
         author: "orange",
         message:
           "It looks like it wraps exactly as it is supposed to. Lets see what a reply looks like!",
-        timestamp: new Date().getTime(),
-      },
+        timestamp: new Date().getTime()
+      }
     ];
     setMessages([...messages, ...tempMessages]);
   };
