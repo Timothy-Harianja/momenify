3fff5b0869d4b6fc3d23aab18552959b1805b407
unknown <724718356@qq.com>
2020-05-09 22:39:05 -0400

    init socket (1)

--- server.js
+++ server.js
@@ -78,15 +78,10 @@ const server = app.listen(API_PORT, () =>
   console.log(`LISTENING ON PORT ${API_PORT}`)
 );
 
-const http = require("http");
-// const server = http.createServer(app);
-
 const socketio = require("socket.io");
 const io = socketio(server);
 io.on("connect", (socket) => {
   console.log("connected to socket.io");
-  // console.log("socket:", socket);
-
   socket.on("disconnect", () => {
     console.log("disconnected");
   });
